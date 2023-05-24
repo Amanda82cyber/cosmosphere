546878e4f0099a971dc77a76c7e3c73a54697268
unknown <adilsonjuniorrocha123@gmail.com>
2023-05-23 21:28:53 -0300

    aaaaaaa

--- Cosmosphere/src/main/java/project/controller/TelaSimulacaoController.java
+++ Cosmosphere/src/main/java/project/controller/TelaSimulacaoController.java
@@ -1,11 +1,34 @@
 package project.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import project.cosmosphere.App;
 
 public class TelaSimulacaoController implements Initializable {
-
+ 
+    @FXML
+    private GridPane TelaSimulacao;
+    
+    
+    @FXML
+    public void voltarTelaInicialSimulacao() throws IOException {
+        URL url = App.class.getResource("TelaInicial.fxml");
+        FXMLLoader fxml = new FXMLLoader(url);
+        Parent raiz = fxml.load();
+        
+        Stage stage = (Stage)TelaSimulacao.getScene().getWindow();
+        stage.setScene(new Scene(raiz));
+        stage.setTitle("Cosmosphere - Tela Inicial");
+        stage.show();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
