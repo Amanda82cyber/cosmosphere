b02464a808ead43c5df6c1d33e1e959b76e9ee68
João Vitor Ribeiro Maciel <joaovitor_10-98@hotmail.com>
2023-05-30 07:37:04 -0300

    Classe Terra quase pronta

---
+++ Cosmosphere/src/main/java/project/cosmosphere/Terra.java
@@ -0,0 +1,21 @@
+package project.cosmosphere;
+
+import java.util.ArrayList;
+
+public class Terra extends Planetas {
+    
+    private String diametro = "12.756,2 Km";
+    private String area = "510.072.000 Km²";
+    private String massa = "5,9736 * 10^24";
+    private double distanciaSol = 149600000; // km
+    private String sateliteNatural = "Lua";
+    private int periodoRotacao = 86164; // segundos
+    private int periodoTranslacao = 31556880; // segundos
+    private String temperatura = "14ºC";
+    private String populacao = "8.028.091.456";
+    private String idade = "4,5 bilhões de anos";
+    private double perimetro = 40075; // km
+    public String [] caracteristicas = {"A Terra é conhecida por “Planeta Azul”, já que cerca de 70% da superfície terrestre é formada por oceanos e mares. A existência da água em seu estado líquido, juntamente à presença do oxigênio oferece à Terra características únicas em comparação a outros planetas", "A Terra é o único planeta que não foi batizado com nome em referência a algum deus romano. Em latim, “terra” significa “solo, região, país”", "A cada 100 anos, a velocidade de rotação da Terra diminui 17 milissegundos", "A Terra tem cerca de 4,5 bilhões de anos, mas a vida só começou a surgir há cerca de 3,8 bilhões de anos", "A Terra é o planeta com maior densidade do Sistema Solar, tendo a densidade 5.513 ", "f"};
+    private String urlImagem;
+    
+}
