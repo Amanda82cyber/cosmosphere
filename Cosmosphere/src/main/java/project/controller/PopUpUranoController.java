9c633d4fb18d6b9327116ed22985457ec43a012b
unknown <adilsonjuniorrocha123@gmail.com>
2023-05-30 01:21:07 -0300

    criei botao botao/espaço de curiosidades

--- Cosmosphere/src/main/java/project/controller/PopUpUranoController.java
+++ Cosmosphere/src/main/java/project/controller/PopUpUranoController.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import project.cosmosphere.App;
 
 public class PopUpUranoController implements Initializable {
     @FXML
@@ -17,6 +21,21 @@ public class PopUpUranoController implements Initializable {
         Stage stage = (Stage) PopUpUrano.getScene().getWindow();
         stage.close();
     }
+    @FXML
+    public void AbrirCuriosidadesUrano() throws IOException {
+        URL url = App.class.getResource("PopUpUrano.fxml");
+        FXMLLoader fxml = new FXMLLoader(url);
+        Parent raiz = fxml.load();
+        
+        Stage stage = (Stage) PopUpUrano.getScene().getWindow();
+        stage.setScene(new Scene(raiz));
+        stage.setTitle("Cosmosphere - curiosidades Mercurio");
+        stage.show();
+    
+    }  
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
