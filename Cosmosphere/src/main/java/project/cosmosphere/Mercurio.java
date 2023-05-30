a5d9311bd7d750bf31aa8f1afc7116b79d85dab7
JOÃO VITOR RIBEIRO MACIEL <bv3015505@AD.SBV.IFSP.EDU.BR>
2023-05-30 15:32:42 -0300

    Criei a classe Mercurio

---
+++ Cosmosphere/src/main/java/project/cosmosphere/Mercurio.java
@@ -0,0 +1,19 @@
+package project.cosmosphere;
+
+public class Mercurio extends Planetas {
+    
+    private String diametro = "4.879,4 Km";
+    private String area = "74.800.000 Km²";
+    private String massa = "3,285 * 10^23 kg";
+    private double distanciaSol = 57900000; // km
+    private String sateliteNatural = "Não possui";
+    private int periodoRotacao = 5097600; // segundos
+    private int periodoTranslacao = 7603200; // segundos
+    private String temperatura = "179ºC";
+    private String populacao = "Não conhecida";
+    private String idade = "4,5 bilhões de anos";
+    private double perimetro = 0; // km (Não encontrei)
+    public String [] curiosidades = {};
+    private String urlImagem = "@../img/mercurio.png";
+    
+}
--- Cosmosphere/src/main/java/project/cosmosphere/Terra.java
+++ Cosmosphere/src/main/java/project/cosmosphere/Terra.java
@@ -1,12 +1,10 @@
 package project.cosmosphere;
 
-import java.util.ArrayList;
-
 public class Terra extends Planetas {
     
     private String diametro = "12.756,2 Km";
     private String area = "510.072.000 Km²";
-    private String massa = "5,9736 * 10^24";
+    private String massa = "5,9736 * 10^24 kg";
     private double distanciaSol = 149600000; // km
     private String sateliteNatural = "Lua";
     private int periodoRotacao = 86164; // segundos
@@ -15,7 +13,14 @@ public class Terra extends Planetas {
     private String populacao = "8.028.091.456";
     private String idade = "4,5 bilhões de anos";
     private double perimetro = 40075; // km
-    public String [] caracteristicas = {"A Terra é conhecida por “Planeta Azul”, já que cerca de 70% da superfície terrestre é formada por oceanos e mares. A existência da água em seu estado líquido, juntamente à presença do oxigênio oferece à Terra características únicas em comparação a outros planetas", "A Terra é o único planeta que não foi batizado com nome em referência a algum deus romano. Em latim, “terra” significa “solo, região, país”", "A cada 100 anos, a velocidade de rotação da Terra diminui 17 milissegundos", "A Terra tem cerca de 4,5 bilhões de anos, mas a vida só começou a surgir há cerca de 3,8 bilhões de anos", "A Terra é o planeta com maior densidade do Sistema Solar, tendo a densidade 5.513 ", "f"};
-    private String urlImagem;
+    public String [] curiosidades = {
+        "A Terra é conhecida por “Planeta Azul”, já que cerca de 70% da superfície terrestre é formada por oceanos e mares. A existência da água em seu estado líquido, juntamente à presença do oxigênio oferece à Terra características únicas em comparação a outros planetas.", 
+        "A Terra é o único planeta que não foi batizado com nome em referência a algum deus romano. Em latim, “terra” significa “solo, região, país”.", 
+        "A cada 100 anos, a velocidade de rotação da Terra diminui 17 milissegundos.", 
+        "A Terra tem cerca de 4,5 bilhões de anos, mas a vida só começou a surgir há cerca de 3,8 bilhões de anos.", 
+        "A Terra é o planeta com maior densidade do Sistema Solar, tendo a densidade 5,51 g/cm³.", 
+        "A distância entre a Lua e a Terra é de cerca de 384.400 km, o que equivale à mais de 30 vezes o diâmetro da Terra."
+    };
+    private String urlImagem = "@../img/terra.png";
     
 }
