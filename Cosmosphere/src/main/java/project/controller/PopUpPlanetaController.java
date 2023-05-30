ecbf5b5788a9e4d9997d6b3b1def55d4dc94584a
Amanda Moreira <nandamoreira945@gmail.com>
2023-05-30 15:30:44 -0300

    Começando a unificar os pop-ups de planeta

--- Cosmosphere/src/main/java/project/controller/PopUpPlanetaController.java
+++ Cosmosphere/src/main/java/project/controller/PopUpPlanetaController.java
@@ -2,9 +2,14 @@ package project.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import project.cosmosphere.Planetas;
 
-public class PopUpPlanetaController implements Initializable {
+public class PopUpPlanetaController implements Initializable { 
+  public void preencherTela(Planetas planeta) {
+    
+  }
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
--- Cosmosphere/src/main/java/project/cosmosphere/Terra.java
+++ Cosmosphere/src/main/java/project/cosmosphere/Terra.java
@@ -15,7 +15,14 @@ public class Terra extends Planetas {
     private String populacao = "8.028.091.456";
     private String idade = "4,5 bilhões de anos";
     private double perimetro = 40075; // km
-    public String [] caracteristicas = {"A Terra é conhecida por “Planeta Azul”, já que cerca de 70% da superfície terrestre é formada por oceanos e mares. A existência da água em seu estado líquido, juntamente à presença do oxigênio oferece à Terra características únicas em comparação a outros planetas", "A Terra é o único planeta que não foi batizado com nome em referência a algum deus romano. Em latim, “terra” significa “solo, região, país”", "A cada 100 anos, a velocidade de rotação da Terra diminui 17 milissegundos", "A Terra tem cerca de 4,5 bilhões de anos, mas a vida só começou a surgir há cerca de 3,8 bilhões de anos", "A Terra é o planeta com maior densidade do Sistema Solar, tendo a densidade 5.513 ", "f"};
+    public String [] caracteristicas = {
+      "A Terra é conhecida por “Planeta Azul”, já que cerca de 70% da superfície terrestre é formada por oceanos e mares. A existência da água em seu estado líquido, juntamente à presença do oxigênio oferece à Terra características únicas em comparação a outros planetas", 
+      "A Terra é o único planeta que não foi batizado com nome em referência a algum deus romano. Em latim, “terra” significa “solo, região, país”", 
+      "A cada 100 anos, a velocidade de rotação da Terra diminui 17 milissegundos", 
+      "A Terra tem cerca de 4,5 bilhões de anos, mas a vida só começou a surgir há cerca de 3,8 bilhões de anos", 
+      "A Terra é o planeta com maior densidade do Sistema Solar, tendo a densidade 5.513 ", 
+      "f"
+    };
     private String urlImagem;
     
 }
