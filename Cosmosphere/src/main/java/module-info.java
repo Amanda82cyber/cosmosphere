54d946df5289ccb9dcddb89ab73e566aa376b923
João Vitor Ribeiro Maciel <joaovitor_10-98@hotmail.com>
2023-05-31 17:10:11 -0300

    Créditos abrindo Linkedingit add .git add .

--- Cosmosphere/src/main/java/module-info.java
+++ Cosmosphere/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module project.cosmosphere {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.desktop;
 
     opens project.cosmosphere to javafx.fxml;
     opens project.controller to javafx.fxml;
